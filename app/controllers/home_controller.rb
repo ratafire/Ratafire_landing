33a82ba9d211145b3b6eca91b44b0715ef808303
fariliang <fariliang@gmail.com>
2015-12-23 01:52:05 -0500

    init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+#This controller controls functions related to homepage
+
+	layout 'static_pages'
+
+	def index
+	end
+
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,16 @@
+module ApplicationHelper
+
+#------------------------ Essentials ------------------------
+
+	#Returns the full title on a per-page basis
+	def full_title(page_title)
+		base_title = "Ratafire"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} - #{page_title}"
+		end
+	end
+
+
+end
