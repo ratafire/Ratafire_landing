9b75a493dd338e029a63a6bb6f4e738bf12f4abd
fariliang <fariliang@gmail.com>
2015-12-24 15:07:24 -0500

    middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.middleware.use Rack::HostRedirect, {
-    'ibaraki-3539.herokussl.com' => 'ratafire.com'
+    'www.ratafire.com' => 'ratafire.com'
   }
 
   # Code is not reloaded between requests.
