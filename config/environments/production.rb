410c7d6fbeea7afbc4d95e2c30f4b5762880063a
fariliang <fariliang@gmail.com>
2015-12-24 15:52:01 -0500

    nowww

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-      r301 %r{.*}, 'https://ratafire.com$&', :if => Proc.new {|rack_env|
-  rack_env['SERVER_NAME'] != 'ratafire.com'
-}
+    r301 /.*/,  Proc.new {|path, rack_env| "https://#{rack_env['SERVER_NAME'].gsub(/www\./i, '') }#{path}" },
+    :if => Proc.new {|rack_env| rack_env['SERVER_NAME'] =~ /www\./i}
   end
 
   # Code is not reloaded between requests.
